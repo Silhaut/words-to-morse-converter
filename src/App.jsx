d6bb65e96c3f4e82ed685f65ea0a504cfb59ca0e
Silhaut <mnstirit@gmail.com>
2023-12-27 15:48:03 +0600

    feat: morse code converter logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const morseCodeMap = {
+	A: '.- ',
+	B: '-... ',
+	C: '-.-. ',
+	D: '-.. ',
+	E: '. ',
+	F: '..-. ',
+	G: '--. ',
+	H: '.... ',
+	I: '.. ',
+	J: '.--- ',
+	K: '-.- ',
+	L: '.-.. ',
+	M: '-- ',
+	N: '-. ',
+	O: '--- ',
+	P: '.--. ',
+	Q: '--.- ',
+	R: '.-. ',
+	S: '... ',
+	T: '- ',
+	U: '..- ',
+	V: '...- ',
+	W: '.-- ',
+	X: '-..- ',
+	Y: '-.-- ',
+	Z: '--.. ',
+	1: '.---- ',
+	2: '..--- ',
+	3: '...-- ',
+	4: '....- ',
+	5: '..... ',
+	6: '-.... ',
+	7: '--... ',
+	8: '---.. ',
+	9: '----. ',
+	0: '----- ',
+	' ': '  /  ',
 }
 
-export default App
+export default function App() {
+	const [inputText, setInputText] = useState('')
+	const [convertedMorse, setConvertedMorse] = useState('')
+
+	const convertToMorse = () => {
+		const upperCaseInput = inputText.toUpperCase()
+		const morseResult = Array.from(upperCaseInput)
+			.map(char => morseCodeMap[char] || '') // If the character is not in the map, ignore it
+			.join('')
+		setConvertedMorse(morseResult)
+	}
+
+	const handleInputChange = inputed => {
+		setInputText(inputed.target.value)
+	}
+
+	const handleSubmit = inputed => {
+		inputed.preventDefault()
+		convertToMorse()
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='morse-code-input'></label>
+				<input
+					type='text'
+					id='morse-code-input'
+					placeholder='Enter here...'
+					value={inputText}
+					onChange={handleInputChange}
+				/>
+				<button type='submit'>Convert to morse</button>
+			</form>
+			<p>Converted morse code: {convertedMorse}</p>
+		</>
+	)
+}

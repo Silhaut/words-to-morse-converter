28cefc53e591f60e155a119865c7d4fe7d369587
Silhaut <mnstirit@gmail.com>
2023-12-29 00:44:40 +0600

    refactor: Converter.jsx component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './css/App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-/* import MorseToWords from './components/MorseToWords'
-import WordsToMorse from './components/WordsToMorse' */
 import Converter from './components/Converter'
 
 export default function App() {

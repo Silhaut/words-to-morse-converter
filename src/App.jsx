9082ebd972ea0cf56583a730148c1efc6aadcf9b
Silhaut <mnstirit@gmail.com>
2023-12-29 00:20:53 +0600

    feat: result now appears in placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import MorseToWords from './components/MorseToWords'
-import WordsToMorse from './components/WordsToMorse'
+import './css/App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+/* import MorseToWords from './components/MorseToWords'
+import WordsToMorse from './components/WordsToMorse' */
+import Converter from './components/Converter'
 
 export default function App() {
 	return (
 		<>
 			<h1>Morse Code converter</h1>
 			<div className='converters'>
-				<WordsToMorse />
-				<MorseToWords />
+				<Converter />
 			</div>
 		</>
 	)
